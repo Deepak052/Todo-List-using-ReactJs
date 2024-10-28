953de3a4a3b3152de4f06939ca04a9dffa907134
Deepak Mehta <dmehta052@gmail.com>
2024-10-28 20:42:00 +0530

    Update vite.config 2nd time for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/to-do-app-using-reactjs/", // Add this line with your repository name
+  base: "/Todo-List-using-ReactJs/", 
 });
